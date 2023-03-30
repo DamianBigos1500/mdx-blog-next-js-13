9d9a94eba9d5ebff5043e9a528f79f8b81d31304
DamianBigos <damianbigos1500@gmail.com>
2023-03-30 12:02:50 +0200

    add react-mdx card

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,21 +1,23 @@
+'server only'
+
 import postsService from 'src/services/posts.service';
 import MdxContent from '@/components/mdxContent/MdxContent';
 import styles from '@/styles/components/posts/post.module.scss';
 import BlogPostHeader from '@/components/blogPost/BlogPostHeader';
 
-// export const generateMetadata = async ({ params }: any) => {
-//   const { data } = postsService.getPostBySlug(params.slug);
-//   return { title: data.title };
-// };
+export const generateMetadata = async ({ params }: any) => {
+  const { data } = postsService.getPostBySlug(params.slug);
+  return { title: data.title };
+};
 
 const page = async ({ params }: { params: { slug: string } }) => {
-  const data = postsService.getPostBySlug(params.slug);
-
-  console.log(data);
+  const { data, content, readingTime } = postsService.getPostBySlug(
+    params.slug
+  );
 
   return (
     <div className={styles.blog_post__container}>
-      {/* <BlogPostHeader
+      <BlogPostHeader
         src={data.imageUrl}
         title={data.title}
         readingTime={readingTime}
@@ -30,7 +32,7 @@ const page = async ({ params }: { params: { slug: string } }) => {
         <div className={styles.blog_post__mdx}>
           <MdxContent source={content} data={data} />
         </div>
-      </div> */}
+      </div>
     </div>
   );
 };
