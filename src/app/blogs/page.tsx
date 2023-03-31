665f4c24d2c14b042f4f16449419dde179ba860a
DamianBigos <damianbigos1500@gmail.com>
2023-04-01 00:34:10 +0200

    refactor

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -2,15 +2,18 @@ import BlogPostCard from '@/components/blogPostCard/BlogPostCard';
 import { BlogPost } from '@/utils/types';
 import postsService from 'src/services/posts.service';
 import styles from '@/styles/components/posts/posts.module.scss';
+import ClientOnly from '@/components/oncyClient/onlyClient';
 
 const page = async () => {
   const blogPosts: BlogPost[] = postsService.getPosts();
 
   return (
     <section className={styles.posts}>
-      {blogPosts.map((post: BlogPost) => (
-        <BlogPostCard key={post.slug} post={post} />
-      ))}
+      <ClientOnly>
+        {blogPosts.map((post: BlogPost) => (
+          <BlogPostCard key={post.slug} post={post} />
+        ))}
+      </ClientOnly>
     </section>
   );
 };
