3453e9740dfa5ba1ebd0323d8e2a8849cd8e6837
DamianBigos <damianbigos1500@gmail.com>
2023-04-01 00:02:57 +0200

    refactor

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -1,13 +1,22 @@
 'use client';
+
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, useEffect, useState } from 'react';
 
 interface ProvidersProps {
   children: ReactNode;
 }
 
 const Providers: FC<ProvidersProps> = ({ children }) => {
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) return null;
+
   return (
     <SessionProvider>
       <ThemeProvider
