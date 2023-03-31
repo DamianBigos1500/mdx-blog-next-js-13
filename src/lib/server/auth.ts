1452b32c43cb57ba7913ff717ea58f99d5f039fb
DamianBigos <damianbigos1500@gmail.com>
2023-04-01 00:29:20 +0200

    refactor

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,4 +1,4 @@
-import { NextAuthOptions } from 'next-auth';
+import { AuthOptions, NextAuthOptions } from 'next-auth';
 import { db } from '@/lib/server/db';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import GoogleProvider from 'next-auth/providers/google';
@@ -40,56 +40,78 @@ function getGithubCredentials() {
   };
 }
 
-export const authOptions: NextAuthOptions = {
+// export const authOptions: NextAuthOptions = {
+//   adapter: PrismaAdapter(db),
+//   session: {
+//     strategy: 'jwt',
+//   },
+//   pages: {
+//     signIn: '/',
+//   },
+//   providers: [
+//     GoogleProvider({
+//       clientId: getGoogleCredentials().clientId,
+//       clientSecret: getGoogleCredentials().clientSecret,
+//     }),
+//     GithubProvider({
+//       clientId: getGithubCredentials().clientId,
+//       clientSecret: getGithubCredentials().clientSecret,
+//     }),
+//   ],
+//   callbacks: {
+//     async session({ token, session }) {
+//       if (token) {
+//         session.user.id = token.id;
+//         session.user.name = token.name;
+//         session.user.email = token.email;
+//         session.user.image = token.picture;
+//       }
+
+//       return session;
+//     },
+//     async jwt({ token, user }) {
+//       const dbUser = await db.user.findFirst({
+//         where: {
+//           email: token.email,
+//         },
+//       });
+
+//       if (!dbUser) {
+//         token.id = user!.id;
+//         return token;
+//       }
+
+//       return {
+//         id: dbUser.id,
+//         name: dbUser.name,
+//         email: dbUser.email,
+//         picture: dbUser.image,
+//       };
+//     },
+//     redirect() {
+//       return '/';
+//     },
+//   },
+// };
+
+export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(db),
-  session: {
-    strategy: 'jwt',
-  },
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
   pages: {
     signIn: '/',
   },
-  providers: [
-    GoogleProvider({
-      clientId: getGoogleCredentials().clientId,
-      clientSecret: getGoogleCredentials().clientSecret,
-    }),
-    GithubProvider({
-      clientId: getGithubCredentials().clientId,
-      clientSecret: getGithubCredentials().clientSecret,
-    }),
-  ],
-  callbacks: {
-    async session({ token, session }) {
-      if (token) {
-        session.user.id = token.id;
-        session.user.name = token.name;
-        session.user.email = token.email;
-        session.user.image = token.picture;
-      }
-
-      return session;
-    },
-    async jwt({ token, user }) {
-      const dbUser = await db.user.findFirst({
-        where: {
-          email: token.email,
-        },
-      });
-
-      if (!dbUser) {
-        token.id = user!.id;
-        return token;
-      }
-
-      return {
-        id: dbUser.id,
-        name: dbUser.name,
-        email: dbUser.email,
-        picture: dbUser.image,
-      };
-    },
-    redirect() {
-      return '/';
-    },
+  debug: process.env.NODE_ENV === 'development',
+  session: {
+    strategy: 'jwt',
   },
+  secret: process.env.NEXTAUTH_SECRET,
 };
