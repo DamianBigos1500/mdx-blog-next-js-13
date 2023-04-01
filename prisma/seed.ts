f9d7ea168c05c072adeffe821bef409eb9ce7208
DamianBigos <damianbigos1500@gmail.com>
2023-04-01 12:55:07 +0200

    repair vercell bugs

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,26 +4,26 @@ import postsService from 'src/services/posts.service';
 async function main() {
   const posts = postsService.getPosts();
 
-  for (let post of posts) {
-    await prisma.blogPost.create({
-      data: {
-        content: post.content,
-        slug: post.slug,
-        data: {
-          create: {
-            title: post.data.title,
-            imageUrl: post.data.imageUrl,
-            secondImageUrl: post.data.secondImageUrl,
-            blurhash: post.data.blurhash,
-            preparationTime: post.data.preparationTime,
-            description: post.data.description,
-            ingredients: '',
-            publishedAt: post.data.publishedAt,
-          },
-        },
-      },
-    });
-  }
+  // for (let post of posts) {
+  //   await prisma.blogPost.create({
+  //     data: {
+  //       content: post.content,
+  //       slug: post.slug,
+  //       data: {
+  //         create: {
+  //           title: post.data.title,
+  //           imageUrl: post.data.imageUrl,
+  //           secondImageUrl: post.data.secondImageUrl,
+  //           blurhash: post.data.blurhash,
+  //           preparationTime: post.data.preparationTime,
+  //           description: post.data.description,
+  //           ingredients: '',
+  //           publishedAt: post.data.publishedAt,
+  //         },
+  //       },
+  //     },
+  //   });
+  // }
 }
 
 main().then();
