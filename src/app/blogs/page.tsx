7669016b570870f5ad6b1218a61ab5afcd65c1fa
DamianBigos <damianbigos1500@gmail.com>
2023-04-10 15:23:55 +0200

    vercel changes

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -2,18 +2,10 @@ import BlogPostCard from '@/components/blogPostCard/BlogPostCard';
 import styles from '@/styles/components/posts/posts.module.scss';
 import { blogPostService } from 'src/services/blogPost.service';
 import { BlogPost } from '@/utils/types';
-import postsService from 'src/services/posts.service';
 
 const page = async () => {
   const blogPosts: BlogPost[] = await blogPostService.getBlogPosts();
 
-  let data;
-  try {
-    data = postsService.getPosts();
-  } catch (error) {}
-
-  console.log(data);
-  
   return (
     <section className={styles.posts}>
       {blogPosts.map((post: any) => (
