53b27d8ebfa62c3f3e064d01ccac28864b6d7f34
DamianBigos <damianbigos1500@gmail.com>
2023-04-15 20:58:59 +0200

    vercel

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -7,9 +7,9 @@ import IngredientsFilter from '@/components/ingredientsFilter/IngredientsFilter'
 import SearchFilter from '@/components/searchFilter/SearchFilter';
 
 const page = async ({ searchParams }: any) => {
-  // const blogPosts: BlogPost[] = await blogPostService.getBlogPosts(
-  //   searchParams
-  // );
+  const blogPosts: BlogPost[] = await blogPostService.getBlogPosts(
+    searchParams
+  );
   let ingredients;
   try {
     ingredients = await ingredientsService.getIngredients();
@@ -22,9 +22,9 @@ const page = async ({ searchParams }: any) => {
         ingredients={ingredients}
         searchParams={searchParams}
       />
-      {/* {blogPosts.map((post: any) => (
+      {blogPosts.map((post: any) => (
         <BlogPostCard key={post.slug} post={post} />
-      ))} */}
+      ))}
     </section>
   );
 };
